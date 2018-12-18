c7bfae8e6198b1783a1789b0ebb0d6f534d837b1
LigerKing <44989360+LigerKing@users.noreply.github.com>
2018-12-18 10:21:32 +0900

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -13,3 +13,4 @@ insert into member values ('song1234','song','30','010-2222-2222');
 insert into member values ('jung1234','jung','20','010-3333-3333');
 insert into member values ('park1234','park','35','010-4444-4444');
 insert into member values ('lee1234','lee','29','010-5555-5555');
+insert into member values ('jang1234', 'jang', '30', '010-6666-6666');
